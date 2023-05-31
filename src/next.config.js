aeec0a748a9d124461fa7e1f7f02ac97a725cbe6
K.Hashida <k.hashida@gftd.co.jp>
2023-05-31 13:43:12 +0900

    next.config.js edit

--- src/next.config.js
+++ src/next.config.js
@@ -7,13 +7,5 @@ module.exports = {
     unoptimized: true,
   },
   basePath: isProduction ? '/repository-name' : '',
-  output: 'export',
- reactStrictMode: true,
- webpackDevMiddleware: config => {
- config.watchOptions = {
-   poll: 800,
-   aggregateTimeout: 300,
- }
- return config
- },
+  output: 'export'
 }
