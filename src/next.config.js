116472825279a3be65ea2b5f1ebf325b1e2c8609
K.Hashida <k.hashida@gftd.co.jp>
2023-05-31 13:05:08 +0900

    basePath: isProduction ? '/repository-name' : '',

--- src/next.config.js
+++ src/next.config.js
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const isProduction = process.env.NODE_ENV === 'production';
 
-// module.exports = {
-//   images: {
-//     unoptimized: true,
-//   },
-//   basePath: '/hk',
-//   output: 'export',
-// }
 module.exports = {
-  reactStrictMode: true,
-  webpackDevMiddleware: config => {
-  config.watchOptions = {
-    poll: 800,
-    aggregateTimeout: 300,
-  }
-  return config
+    images: {
+    unoptimized: true,
   },
+  basePath: isProduction ? '/repository-name' : '',
+  output: 'export',
+ reactStrictMode: true,
+ webpackDevMiddleware: config => {
+ config.watchOptions = {
+   poll: 800,
+   aggregateTimeout: 300,
  }
+ return config
+ },
+}
